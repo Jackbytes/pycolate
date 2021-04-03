2e72774512f347c828cb55c224f688ab39f763f0
Jack <jackjharrington@icloud.com>
2021-04-03 21:42:13 +0100

    Fixed packaging bug.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pycolate",
-    version="0.0.2",
+    version="0.0.21",
     author="Jack Harrington",
     author_email="jackjharrington@icloud.com",
     description="Generates site percolation data and illustrations.",
